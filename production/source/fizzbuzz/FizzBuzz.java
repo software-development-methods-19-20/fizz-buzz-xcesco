38209ffa70b55e0fa0bb5e3c3e0ae01ed4f8822b
xcesco <abubusoft@gmail.com>
2019-11-21 00:15:54 +0100

    2 - Implement the additional rules for multiples of 7

--- production/source/fizzbuzz/FizzBuzz.java
+++ production/source/fizzbuzz/FizzBuzz.java
@@ -1,23 +1,41 @@
 package fizzbuzz;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
+
+    private static Map<Integer, String> conversionMap;
+
+    {
+        conversionMap = new HashMap<>();
+        conversionMap.put(3, "Fizz");
+        conversionMap.put(5, "Buzz");
+        conversionMap.put(7, "Bang");
+    }
+
     public String execute() {
         Function<Integer, String> converter = number -> {
-            String conversion="";
-            if (number % 3==0) {
-                conversion+="Fizz";
+            String conversion = "";
+
+            for (Map.Entry<Integer, String> item : conversionMap.entrySet()) {
+                conversion = tryToConvertInString(number, conversion, item.getKey(), item.getValue());
             }
-            if (number % 5==0) {
-                conversion+="Buzz";
-            }
-            if (conversion.length()==0) conversion=""+number;
-            return conversion;
+
+            if (conversion.length() == 0) conversion = String.valueOf(number);
+            return "["+conversion+"]";
         };
 
-       return IntStream.rangeClosed(1, 100).mapToObj(converter::apply).collect(Collectors.joining());
+        return IntStream.rangeClosed(1, 100).mapToObj(converter::apply).collect(Collectors.joining());
+    }
+
+    private String tryToConvertInString(Integer number, String currentConversion, int i, String replacedValue) {
+        if (number % i == 0) {
+            currentConversion += replacedValue;
+        }
+        return currentConversion;
     }
 }
--- test/source/test/FizzBuzzTest.java
+++ test/source/test/FizzBuzzTest.java
@@ -15,7 +15,16 @@ public class FizzBuzzTest {
         String result = fizzBuzz.execute();
         System.out.println(result);
 
-        assertThat(result, stringContainsInOrder("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz"));
+        assertThat(result, stringContainsInOrder("[1][2][Fizz][4][Buzz][Fizz][Bang][8][Fizz][Buzz]" +
+                "[11][Fizz][13][Bang][FizzBuzz][16][17][Fizz][19][Buzz]" +
+                "[FizzBang][22][23][Fizz][Buzz][26][Fizz][Bang][29][FizzBuzz]" +
+                "[31][32][Fizz][34][BuzzBang][Fizz][37][38][Fizz][Buzz]" +
+                "[41][FizzBang][43][44][FizzBuzz][46][47][Fizz][Bang]" +
+                "[Buzz][Fizz][52][53][Fizz][Buzz][Bang][Fizz][58][59][FizzBuzz]" +
+                "[61][62][FizzBang][64][Buzz][Fizz][67][68][Fizz][BuzzBang]" +
+                "[71][Fizz][73][74][FizzBuzz][76][Bang][Fizz][79][Buzz]" +
+                "[Fizz][82][83][FizzBang][Buzz][86][Fizz][88][89][FizzBuzz]" +
+                "[Bang][92][Fizz][94][Buzz][Fizz][97][Bang][Fizz][Buzz]"));
 
     }
 }
